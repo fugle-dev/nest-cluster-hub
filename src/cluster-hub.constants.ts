b6818a4c0f25a525f886b8e0061f302843d7bdc6
Chun-Kai Wang <chunkai1312@gmail.com>
2022-06-19 02:36:26 +0800

    chore: constants

--- src/cluster-hub.constants.ts
+++ src/cluster-hub.constants.ts
@@ -1,3 +1,3 @@
 export const CLUSTER_HUB = Symbol('CLUSTER_HUB');
-export const LOCALS_GET_EVENT = Symbol('locals:get');
-export const LOCALS_SET_EVENT = Symbol('locals:set');
+export const LOCALS_GET_EVENT = '_locals:get';
+export const LOCALS_SET_EVENT = '_locals:set';
