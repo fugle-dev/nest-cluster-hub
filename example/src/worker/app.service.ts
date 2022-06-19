b6815e1b41b4bfec8e6d75c1156b30c7f64f0bcf
Chun-Kai Wang <chunkai1312@gmail.com>
2022-06-19 22:26:07 +0800

    refactor: set listener with decorator

--- example/src/worker/app.service.ts
+++ example/src/worker/app.service.ts
@@ -1,13 +1,10 @@
-import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
-import { InjectClusterHub, ClusterHub } from 'nest-cluster-hub';
+import { Injectable } from '@nestjs/common';
+import { OnRequest } from 'nest-cluster-hub';
 
 @Injectable()
-export class AppService implements OnApplicationBootstrap {
-  constructor(@InjectClusterHub() private readonly hub: ClusterHub) {}
-
-  async onApplicationBootstrap() {
-    this.hub.on('sum', (data: number[], sender, callback) => {
-      callback(null, (data || []).reduce((a, b) => a + b));
-    });
+export class AppService {
+  @OnRequest('sum')
+  handleRequest(data: number[], sender, callback) {
+    callback(null, (data || []).reduce((a, b) => a + b));
   }
 }
