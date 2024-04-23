9bd90c035402af0121744478d8edbfbd693db43b
Chun-Kai Wang <chunkai1312@gmail.com>
2024-04-23 15:15:59 +0800

    feat: add support for waiting until the specified number of workers is ready

--- src/cluster-hub.ts
+++ src/cluster-hub.ts
@@ -1,14 +1,19 @@
 import * as cluster from 'cluster';
+import * as os from 'os';
 import * as Hub from 'cluster-hub';
 import * as HashRing from 'hashring';
+import { ClusterHubModuleOptions } from './interfaces/cluster-hub-module-options.interface';
 import { LOCALS_GET_EVENT, LOCALS_SET_EVENT } from './cluster-hub.constants';
 
 export class ClusterHub extends Hub {
   private static instance: ClusterHub;
   private ring: HashRing;
   private locals: Map<string, any>;
+  private numWorkers: number;
+  private numWorkersReady: number;
+  private workersReadyCallback?: () => void;
 
-  constructor() {
+  constructor(private options?: ClusterHubModuleOptions) {
     super();
 
     if (cluster.isMaster) {
@@ -17,9 +22,9 @@ export class ClusterHub extends Hub {
     }
   }
 
-  public static getInstance(): ClusterHub {
+  public static getInstance(options?: ClusterHubModuleOptions): ClusterHub {
     if (!ClusterHub.instance) {
-      ClusterHub.instance = new ClusterHub();
+      ClusterHub.instance = new ClusterHub(options);
     }
     return ClusterHub.instance;
   }
@@ -58,11 +63,29 @@ export class ClusterHub extends Hub {
     return cluster.workers;
   }
 
+  waitWorkers(callback: () => void): void {
+    if (cluster.isWorker) {
+      throw new Error('The current process is not a primary');
+    }
+    if (this.numWorkersReady === this.numWorkers) {
+      callback();
+    } else {
+      this.workersReadyCallback = callback;
+    }
+  }
+
   private setupWorkers() {
     this.ring = new HashRing([]);
+    this.numWorkers = this.options?.workers ?? os.cpus().length;
+    this.numWorkersReady = 0;
 
     cluster.on('online', (worker) => {
       this.ring.add(`${worker.id}`);
+      this.numWorkersReady++;
+
+      if (this.workersReadyCallback && this.numWorkersReady === this.numWorkers) {
+        this.workersReadyCallback();
+      }
     });
 
     cluster.on('exit', (worker) => {
